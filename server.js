40a814d66d2b160bbe05fe78904ec20a0991ecf1
iamvishal345 <vishalagnihotri345@gmail.com>
2020-09-22 07:54:04 +0530

    Updating app to be a PWA

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const compression = require("compression");
-
+const enforce = require("express-sslify");
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
@@ -12,6 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustAzureHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
@@ -26,6 +27,9 @@ app.listen(port, (err) => {
   console.log("Server running on port ", port);
 });
 
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "service-worker.js"));
+});
 app.post("/payment", (req, res) => {
   const body = {
     source: req.body.token.id,
